12b37c743c6970ab90c5c70df1b61e3d4692770c
Hjubong <juju814@naver.com>
2023-11-30 19:58:00 +0900

    요금

--- src/main/java/com/project/dd/pb/price/domain/PriceDTO.java
+++ src/main/java/com/project/dd/pb/price/domain/PriceDTO.java
@@ -1,5 +1,13 @@
 package com.project.dd.pb.price.domain;
 
+import lombok.Data;
+
+@Data
 public class PriceDTO {
 
+	private String ticket_seq;
+	private String ticket_type;
+	private String person_type;
+	private String age;
+	private String price;
 }
