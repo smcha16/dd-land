9724c9aad17677780bc19a24377f4e71cc42fbe2
NaraeP <skfocjstk93@naver.com>
2023-12-04 16:57:17 +0900

    Update. 04DEC23 template/list2.jsp 검색란 Select 박스 추가

---
+++ src/main/java/com/project/dd/activity/attraction/controller/MemberAttractionController.java
@@ -0,0 +1,11 @@
+package com.project.dd.activity.attraction.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberAttractionController {
+
+	
+}
