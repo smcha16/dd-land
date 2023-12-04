36e356f974d956b929945acceb7c627a7a0c9f00
dmsalsgus <leeje971118@naver.com>
2023-12-04 15:19:41 +0900

    add : custom.login.success.handler 틀 추가

---
+++ src/main/java/com/project/dd/CustomLoginSuccessHandler.java
@@ -0,0 +1,21 @@
+package com.project.dd;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler{
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		
+	}
+}
