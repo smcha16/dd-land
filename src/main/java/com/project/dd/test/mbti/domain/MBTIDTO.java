7b5f0e37e3ecf17622d3742297f51fd67f7b9a55
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-12-04 16:07:16 +0900

    chore: MBTI DTO추가

--- src/main/java/com/project/dd/test/mbti/domain/MBTIDTO.java
+++ src/main/java/com/project/dd/test/mbti/domain/MBTIDTO.java
@@ -1,5 +1,12 @@
 package com.project.dd.test.mbti.domain;
 
+
 public class MBTIDTO {
 
+	private String mbti_seq;
+	private String result;
+	private String mbti;
+	private String course_seq;
+	private String attraction_seq;
+	
 }
