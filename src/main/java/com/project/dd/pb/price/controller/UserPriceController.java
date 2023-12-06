591c6e660e127534555f421ebcc308a0c3f4c3cd
HendrikJohannesCruijff <qjtjzj2733@naver.com>
2023-12-06 14:08:30 +0900

    요금 금액 뿌리기  ㅓㅋ밋

--- src/main/java/com/project/dd/pb/price/controller/UserPriceController.java
+++ src/main/java/com/project/dd/pb/price/controller/UserPriceController.java
@@ -1,6 +1,11 @@
 package com.project.dd.pb.price.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+
+import java.text.NumberFormat;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,16 +26,49 @@ public class UserPriceController {
 	@GetMapping(value = "/view.do")
 	public String view(Model model) {
 		
-		List<PriceDTO> personTypeList = mapper.personTypeList(); // 개인 티켓 리스
+		List<PriceDTO> personTypeList = mapper.personTypeList(); // 개인 티켓 리스트
 		
-	
+		
+		for(int i=0; i<personTypeList.size(); i++) {  // 가격 ,붙히기
+		
+			int money =  Integer.valueOf(personTypeList.get(i).getPrice()) ;
+			
+			// NumberFormat 객체 생성
+	        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
+
+	        // 숫자를 형식화하여 출력
+	        String formattedMoney = numberFormat.format(money);
+	        
+	        
+	        personTypeList.get(i).setPrice(formattedMoney);
+			
+		}
+		
+		
+		
+		
 		
 		List<PriceDTO> groupTypeList = mapper.groupTypeList(); //단체 티켓 리스트
 		
-		 
-	
+		for(int i=0; i<groupTypeList.size(); i++) { //가격 , 붙히기
+		
+			
+			int money =  Integer.valueOf(groupTypeList.get(i).getPrice()) ;
+			
+			// NumberFormat 객체 생성
+	        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
 
-		model.addAttribute("groupTyepList",groupTypeList);
+	        // 숫자를 형식화하여 출력
+	        String formattedMoney = numberFormat.format(money);
+	        
+	        
+	        personTypeList.get(i).setPrice(formattedMoney);
+			
+		}
+		
+		
+
+		model.addAttribute("groupTypeList",groupTypeList);
 		model.addAttribute("personTypeList",personTypeList);
 		
 		return "user/pb/price/view";
